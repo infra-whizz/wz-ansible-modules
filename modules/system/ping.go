44670d20594257e855cb2eb3d6b5a677e0cc4463
Bo Maryniuk <bo@maryniuk.net>
2020-06-29 15:28:12 +0200

    Add simple ping module

---
+++ modules/system/ping.go
@@ -0,0 +1,26 @@
+// Ping module
+
+package main
+
+import (
+	"fmt"
+
+	wzmodlib "github.com/infra-whizz/wzmodlib"
+)
+
+type ModuleArgs struct {
+	Text string
+}
+
+func main() {
+	var args ModuleArgs
+	response := wzmodlib.CheckModuleCall(args)
+
+	if args.Text != "" {
+		response.Msg = fmt.Sprintf("You say: %s", args.Text)
+	} else {
+		response.Msg = "Pong!"
+	}
+
+	wzmodlib.ExitWithJSON(response)
+}
