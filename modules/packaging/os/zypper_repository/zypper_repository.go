0b638761e26d4ad32bdb718e1c5ab872470a7035
Bo Maryniuk <bo@maryniuk.net>
2020-07-17 18:07:58 +0200

    Add zypper module scaffold

---
+++ modules/packaging/os/zypper_repository/zypper_repository.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+
+	wzmodlib "github.com/infra-whizz/wzmodlib"
+)
+
+/*
+Documentation:
+module: zypper_repository
+author: "Matthias Vogelgesang (@matze)"
+version_added: "1.4"
+short_description: Add and remove Zypper repositories
+description:
+    - Add or remove Zypper repositories on SUSE and openSUSE
+options:
+    name:
+        description:
+            - A name for the repository. Not required when adding repofiles.
+    repo:
+        description:
+            - URI of the repository or .repo file. Required when state=present.
+    state:
+        description:
+            - A source string state.
+        choices: [ "absent", "present" ]
+        default: "present"
+    description:
+        description:
+            - A description of the repository
+    disable_gpg_check:
+        description:
+            - Whether to disable GPG signature checking of
+              all packages. Has an effect only if state is
+              I(present).
+            - Needs zypper version >= 1.6.2.
+        type: bool
+        default: 'no'
+    autorefresh:
+        description:
+            - Enable autorefresh of the repository.
+        type: bool
+        default: 'yes'
+        aliases: [ "refresh" ]
+    priority:
+        description:
+            - Set priority of repository. Packages will always be installed
+              from the repository with the smallest priority number.
+            - Needs zypper version >= 1.12.25.
+    overwrite_multiple:
+        description:
+            - Overwrite multiple repository entries, if repositories with both name and
+              URL already exist.
+        type: bool
+        default: 'no'
+    auto_import_keys:
+        description:
+            - Automatically import the gpg signing key of the new or changed repository.
+            - Has an effect only if state is I(present). Has no effect on existing (unchanged) repositories or in combination with I(absent).
+            - Implies runrefresh.
+            - Only works with C(.repo) files if `name` is given explicitly.
+        type: bool
+        default: 'no'
+    runrefresh:
+        description:
+            - Refresh the package list of the given repository.
+            - Can be used with repo=* to refresh all repositories.
+        type: bool
+        default: 'no'
+    enabled:
+        description:
+            - Set repository to enabled (or disabled).
+        type: bool
+        default: 'yes'
+
+
+Examples:
+
+# Add Ansible repository
+- zypper_repository:
+    repo: 'https://download.opensuse.org/repositories/systemsmanagement/openSUSE_Leap_15.2/'
+
+# Refresh all repos
+- zypper_repository:
+    repo: '*'
+    runrefresh: yes
+
+# Add a repo and add it's gpg key
+- zypper_repository:
+    repo: 'https://download.opensuse.org/repositories/systemsmanagement/openSUSE_Leap_15.2/'
+    auto_import_keys: yes
+
+# Force refresh of a repository
+- zypper_repository:
+    repo: 'https://download.opensuse.org/repositories/systemsmanagement/openSUSE_Leap_15.2/'
+    name:
+    state: present
+    runrefresh: yes
+'''
+
+*/
+
+type ModuleArgs struct {
+	Text string
+}
+
+func main() {
+	var args ModuleArgs
+	response := wzmodlib.CheckModuleCall(args)
+
+	if args.Text != "" {
+		response.Msg = fmt.Sprintf("You say: %s", args.Text)
+	} else {
+		response.Msg = "Pong!"
+	}
+
+	wzmodlib.ExitWithJSON(*response)
+
+}
