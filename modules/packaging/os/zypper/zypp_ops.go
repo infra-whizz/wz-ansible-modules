94748af5ac86dc0dd3ff7a2022661b1ff14be3af
Bo Maryniuk <bo@maryniuk.net>
2020-07-24 15:09:39 +0200

    Refactor and implement removal filtering

--- modules/packaging/os/zypper/zypp_ops.go
+++ modules/packaging/os/zypper/zypp_ops.go
@@ -18,13 +18,48 @@ type ZypperOperations struct {
 func NewZypperOperations() *ZypperOperations {
 	zr := new(ZypperOperations)
 	zr.zypp = NewZypper().XML(true)
-	zr.GetLogger().SetLevel(logrus.InfoLevel)
+	zr.GetLogger().SetLevel(logrus.DebugLevel)
 	return zr
 }
 
 // An alias to YesNo2Bool
 func (zr *ZypperOperations) toBool(val string) bool {
 	return wzmodlib.YesNo2Bool(val)
+}
+
+// FilterNew removes all the packages that are not installed.
+func (zr *ZypperOperations) FilterNew(packages []string) ([]string, error) {
+	installedPackages := make([]string, 0)
+	stout, sterr, err := zr.zypp.New().Search().InstalledOnly().Packages(packages...).Call(zr.args.PipeFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if sterr != "" {
+		return installedPackages, fmt.Errorf("Error: %s", sterr)
+	}
+
+	var search ZypperSearch
+	if err := xml.Unmarshal([]byte(stout), &search); err != nil {
+		panic(err)
+	}
+
+	for _, pkgname := range packages {
+		installed := false
+		for _, solvable := range search.Result.SolvableList.Packages {
+			if pkgname == solvable.Name {
+				installed = true
+				break
+			}
+		}
+
+		if installed {
+			installedPackages = append(installedPackages, pkgname)
+		}
+	}
+
+	return installedPackages, nil
 }
 
 // FilterInstalled cleans up already installed packages.
@@ -59,31 +94,43 @@ func (zr *ZypperOperations) FilterInstalled(packages []string) ([]string, error)
 	return newPackages, nil
 }
 
-func (zr *ZypperOperations) Install(packages ...string) {
+func (zr *ZypperOperations) Install(packages ...string) error {
 	if len(packages) == 0 {
-		return
+		return fmt.Errorf("No packages has been selected for installation")
 	}
+
 	_, sterr, err := zr.zypp.New().Install().Packages(packages...).Call(zr.args.PipeFile)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if sterr != "" {
-		panic(fmt.Errorf("Error: %s", sterr))
+		return fmt.Errorf("Error: %s", sterr)
 	}
+
+	return nil
 }
 
 // Remove installedd packages
-func (zr *ZypperOperations) Remove(packages ...string) {
-	if len(packages) == 0 {
-		return
+func (zr *ZypperOperations) Remove(packages ...string) error {
+	// Throw away new packages (non installed)
+	packages, err := zr.FilterNew(packages)
+	if err != nil {
+		return err
 	}
+
+	if len(packages) == 0 {
+		return fmt.Errorf("No packages has been selected for removal")
+	}
+
 	_, sterr, err := zr.zypp.New().Remove().Packages(packages...).Call(zr.args.PipeFile)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if sterr != "" {
-		panic(fmt.Errorf("Error: %s", sterr))
+		return fmt.Errorf("Error: %s", sterr)
 	}
+
+	return nil
 }
 
 // Configure from arguments
@@ -101,20 +148,19 @@ func (zr *ZypperOperations) Run() (bool, error) {
 		if err != nil {
 			return false, err
 		}
-		zr.Install(packages...)
-		return true, nil
+		if err := zr.Install(packages...); err != nil {
+			return false, err
+		}
 	case "latest":
 		return false, fmt.Errorf("State %s not yet implemented", zr.args.State)
 	case "absent":
-		packages, err := zr.FilterInstalled(zr.args.Packages)
-		if err != nil {
+		if err := zr.Remove(zr.args.Packages...); err != nil {
 			return false, err
 		}
-		zr.Remove(packages...)
-		return false, fmt.Errorf("State %s not yet implemented", zr.args.State)
 	case "dist-upgrade":
 		return false, fmt.Errorf("State %s not yet implemented", zr.args.State)
 	default:
 		return false, fmt.Errorf("Unknown state: %s", zr.args.State) // This should not happen though
 	}
+	return true, nil
 }
